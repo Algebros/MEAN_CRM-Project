472189f29279833c12063233908f2c56bcbb3636
Sofroniy <Sofroniy.m@gmail.com>
2020-05-05 17:02:16 +0300

    feat: order functionality implemented

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,13 +1,46 @@
-const getAll = (req, res) => {
-  res.status(200).json({
-    login: 'login'
-  })
+const Order = require('../models/order');
+
+const getAll = async (req, res) => {
+  const query = {
+    user: req.user.id,
+    
+  }
+  if(req.query.start) {
+    query.date = {
+      $gte: req.query.start
+    }
+  }
+
+  if(req.query.end) {
+    if(!query.date) {
+      query.date = {};
+    }
+    query.date['$lte'] = req.query.end
+  }
+
+  if(req.query.order) {
+    query.order = +req.query.order
+  }
+
+  const order = await Order.find(query)
+  .sort({ date: -1 })
+  .skip(+req.query.offset)
+  .limit(+req.query.limit);
+  res.status(200).json(order);
 }
 
-const create = (req, res) => {
-  res.status(200).json({
-    login: 'reg'
-  })
+const create = async (req, res) => {
+  const lastOrder = await Order.findOne({ user: req.user.id })
+  .sort({ date: -1 });
+
+  const maxOrder = lastOrder ? lastOrder.order : 0;
+
+  const order = await Order.create({
+    list: req.body.list,
+    user: req.user.id,
+    order: maxOrder + 1,
+  });
+  res.status(201).json(order);
 }
 
 module.exports = {
