9c52867eb112c7cfe81740d87c0f84bf7d72eaee
Sofroniy <Sofroniy.m@gmail.com>
2020-05-11 23:45:09 +0300

    feat: categories-page component implemented

--- client/src/app/shared/classes/material.service.ts
+++ client/src/app/shared/classes/material.service.ts
@@ -1,6 +1,7 @@
 import { ElementRef } from '@angular/core';
 
-declare var M: { toast: (arg0: { html: string; }) => void; FloatingActionButton: { init: (arg0: any) => void; }; };
+// tslint:disable-next-line: max-line-length
+declare var M: { toast: (arg0: { html: string; }) => void; FloatingActionButton: { init: (arg0: any) => void; }; updateTextFields: () => void; };
 
 export class MaterialSerice {
   static toast(message: string) {
@@ -9,5 +10,9 @@ export class MaterialSerice {
 
   static initializeFloatingButton(ref: ElementRef) {
     M.FloatingActionButton.init(ref.nativeElement);
+  }
+
+  static updateTextInputs() {
+    M.updateTextFields();
   }
 }
