50303bcaaaab603fcc165a1a4ce8c89740f6da46
Sofroniy <Sofroniy.m@gmail.com>
2020-05-14 19:31:13 +0300

    feat: display orders on the history-list component implemented

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
-import { MaterialInstance } from '../shared/interfaces';
+import { MaterialInstance, Order } from '../shared/interfaces';
 import { MaterialSerice } from '../shared/classes/material.service';
 import { OrderService } from '../shared/services/order.service';
+import { Subscription } from 'rxjs';
+
+const STEP = 2;
 
 @Component({
   selector: 'app-history-page',
@@ -13,20 +16,33 @@ export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild('tooltip') tooltipRef: ElementRef;
   tooltip: MaterialInstance;
   isFilterVisible = false;
+  oSub: Subscription;
+  orders: Order[];
+
+  offset = 0;
+  limit = STEP;
 
   constructor(
     private ordersService: OrderService,
   ) { }
 
   ngOnInit(): void {
+    this.fetch();
   }
 
   private fetch() {
-    this.ordersService.fetch();
+    const params = {
+      offset: this.offset,
+      limit: this.limit
+    };
+    this.oSub = this.ordersService.fetch().subscribe((orders) => {
+      this.orders = orders;
+    });
   }
 
   ngOnDestroy() {
     this.tooltip.destroy();
+    if (this.oSub) { this.oSub.unsubscribe(); }
   }
 
   ngAfterViewInit() {
