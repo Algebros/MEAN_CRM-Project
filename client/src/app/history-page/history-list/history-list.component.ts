d8c673ea7cbead9ece38d8154e6bf1a855a23f68
Sofroniy <Sofroniy.m@gmail.com>
2020-05-15 14:02:08 +0300

    feat: implemented detailed display of orders

--- client/src/app/history-page/history-list/history-list.component.ts
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -1,18 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Order } from 'src/app/shared/interfaces';
+import { Component, Input, ViewChild, ElementRef, OnDestroy, AfterViewInit } from '@angular/core';
+import { Order, MaterialInstance } from 'src/app/shared/interfaces';
+import { MaterialSerice } from 'src/app/shared/classes/material.service';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.scss']
 })
-export class HistoryListComponent implements OnInit {
+export class HistoryListComponent implements OnDestroy, AfterViewInit {
 
   @Input() orders: Order[];
+  @ViewChild('modal') modalRef: ElementRef;
+
+  selectedOrder: Order;
+  modal: MaterialInstance;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  ngAfterViewInit() {
+    this.modal = MaterialSerice.initModal(this.modalRef);
+  }
+
+  computePrice(order: Order): number {
+    return order.list.reduce((total, item) => total += item.quantity * item.cost, 0);
+  }
+
+  selectOrder(order: Order) {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  closeModal() {
+    this.modal.close();
   }
 
 }
